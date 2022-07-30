120c140c42bfa97ae3a7a13de2b2c34766305c4a
Fisatidi Andrey <fissatidi@gmail.ru>
2022-07-30 20:55:21 +0500

    Выполнено

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Задание 1
+        {
+            String firstName = " Ivan";
+
+            String middleName = " Ivanovich";
+            String lastName = " Ivanov";
+            String fullName = lastName + firstName + middleName;
+            System.out.println("ФИО сотрудника -  " + fullName);
+        }
+
+        // Задание 2
+
+        {
+            String fullName = "Ivanov Ivan Ivanovich";
+            String fullNameUpper = fullName.toUpperCase();
+            System.out.println("Данные ФИО сотрудника для заполнения отчета -  " + fullNameUpper);
+        }
+        // Задание 3
+
+        {
+
+            String fullName = "Иванов Семён Семёнович";
+            fullName = fullName.replace("ё", "е");
+            fullName = fullName.replace("Ё", "Е");
+            System.out.println("Данные ФИО сотрудника - " + fullName);
+        }
     }
 }
